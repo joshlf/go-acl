7226979b7d387108498a713efc437e05544f248c
Joshua Liebow-Feeser <hello@synful.io>
2015-08-20 17:31:30 -0700

    Added new functionality

--- acl.go
+++ acl.go
@@ -1,12 +1,22 @@
-// Package acl implements POSIX.1e-compliant
+// Package acl implements POSIX.1e draft 17-compliant
 // manipulation of access control lists (ACLs).
 // See the acl manpage for details: http://linux.die.net/man/5/acl
+//
+// This package links against libacl. By default,
+// it links statically, as libacl is not installed
+// on some systems. To have it link dynamically,
+// build with the "acl_link_dynamic" tag.
+//
+// NOTE: Some versions of Darwin's libacl have a bug;
+// users compiling for Darwin should be very careful
+// about linking dynamically.
 package acl
 
 import (
 	"C"
 	"fmt"
 	"os"
+	"strings"
 	"unsafe"
 )
 
@@ -25,41 +35,178 @@ import (
 		  form.
 */
 
+// ACL represents an access control list as defined
+// in the POSIX.1e draft standard. If an ACL is not
+// valid (see the IsValid method), the behavior of
+// the functions and methods of this package is
+// undefined.
 type ACL []Entry
 
-type Tag tag
-
-const (
-	TagUndefined Tag = tagUndefined
-	TagUserObj       = tagUserObj  // Permissions of the file owner
-	TagUser          = tagUser     // Permissions of a specified user
-	TagGroupObj      = tagGroupObj // Permissions of the file group
-	TagGroup         = tagGroup    // Permissions of a specified group
-	TagMask          = tagMask     // Maximum allowed access rights of any entry
-	TagOther         = tagOther    // Permissions of a process not matching any other entry
-)
-
-func (t Tag) String() string {
-	switch t {
-	case TagUndefined:
-		return "TagUndefined"
-	case TagUserObj:
-		return "TagUserObj"
-	case TagUser:
-		return "TagUser"
-	case TagGroupObj:
-		return "TagGroupObj"
-	case TagGroup:
-		return "TagGroup"
-	case TagMask:
-		return "TagMask"
-	case TagOther:
-		return "TagOther"
-	default:
-		return fmt.Sprintf("UnknownTag(%v)", tag(t))
+// FromUnix generates an ACL equivalent to the given
+// unix permissions bitmask. All non-permission bits
+// in perms are ignored.
+func FromUnix(perms os.FileMode) ACL {
+	return ACL{
+		{Tag: TagUserObj, Perms: (perms >> 6) & 7},
+		{Tag: TagGroupObj, Perms: (perms >> 3) & 7},
+		{Tag: TagOther, Perms: perms & 7},
 	}
 }
 
+// ToUnix returns the unix permissions bitmask
+// encoded by a. If a is not valid as defined
+// by a.IsValid, the behavior of ToUnix is
+// undefined.
+func ToUnix(a ACL) os.FileMode {
+	var perms os.FileMode
+	for i := 0; i < 3 && i < len(a); i++ {
+		perms <<= 3
+		perms |= a[i].perms()
+	}
+	return perms
+}
+
+// IsValid returns whether a is a valid ACL as defined
+// by the POSIX.1e draft standard.
+func (a ACL) IsValid() bool {
+	var numUserObj, numGroupObj, numOther int
+	var numMask, numUserOrGroup int
+	users := make(map[string]bool)
+	groups := make(map[string]bool)
+	for _, e := range a {
+		switch e.Tag {
+		case TagUserObj:
+			numUserObj++
+		case TagGroupObj:
+			numGroupObj++
+		case TagOther:
+			numOther++
+		case TagMask:
+			numMask++
+		case TagUser:
+			numUserOrGroup++
+			if users[e.Qualifier] {
+				return false
+			}
+			users[e.Qualifier] = true
+		case TagGroup:
+			numUserOrGroup++
+			if groups[e.Qualifier] {
+				return false
+			}
+			groups[e.Qualifier] = true
+		default:
+			return false
+		}
+	}
+	switch {
+	case numUserObj != 1:
+		return false
+	case numGroupObj != 1:
+		return false
+	case numOther != 1:
+		return false
+	case numUserOrGroup > 0 && numMask == 0:
+		return false
+	case numMask > 1:
+		return false
+	}
+	return true
+}
+
+// String implements the POSIX.1e short text form.
+// For example:
+//  u::rwx,g::r-x,o::--,u:dvader:r--,m::r--
+// This output is produced by an ACL in which the file owner
+// has read, write, and execute; the file group has read and
+// execute; other has no permissions; the user dvader has
+// read; and the mask is read.
+func (a ACL) String() string {
+	strs := make([]string, len(a))
+	for i, e := range a {
+		strs[i] = e.String()
+	}
+	return strings.Join(strs, ",")
+}
+
+// StringLong implements the POSIX.1e long text form.
+// The long text form of the example given above is:
+//  user::rwx
+//  group::r-x
+//  other::---
+//  user:dvader:r--
+//  mask::r--
+func (a ACL) StringLong() string {
+	lines := make([]string, len(a))
+	mask := os.FileMode(7)
+	for _, e := range a {
+		if e.Tag == TagMask {
+			mask = e.perms()
+			break
+		}
+	}
+	for i, e := range a {
+		if (e.Tag == TagUser || e.Tag == TagGroupObj || e.Tag == TagGroup) &&
+			mask|e.perms() != mask {
+			effective := mask & e.perms()
+			lines[i] = fmt.Sprintf("%-20s#effective:%s", e.StringLong(), permString(effective))
+		} else {
+			lines[i] = e.StringLong()
+		}
+	}
+	return strings.Join(lines, "\n")
+}
+
+// Tag is the type of an ACL entry tag.
+type Tag tag
+
+const (
+	TagUserObj  Tag = tagUserObj  // Permissions of the file owner
+	TagUser         = tagUser     // Permissions of a specified user
+	TagGroupObj     = tagGroupObj // Permissions of the file group
+	TagGroup        = tagGroup    // Permissions of a specified group
+
+	// Maximum allowed access rights of any entry
+	// with the tag TagUser, TagGroupObj, or TagGroup
+	TagMask  = tagMask
+	TagOther = tagOther // Permissions of a process not matching any other entry
+)
+
+// String implements the POSIX.1e short text form.
+func (t Tag) String() string {
+	switch t {
+	case TagUser, TagUserObj:
+		return "u"
+	case TagGroup, TagGroupObj:
+		return "g"
+	case TagOther:
+		return "o"
+	case TagMask:
+		return "m"
+	default:
+		// TODO(synful): what to do in this case?
+		return "?" // non-standard, but not specified in POSIX.1e
+	}
+}
+
+// StringLong implements the POSIX.1e long text form.
+func (t Tag) StringLong() string {
+	switch t {
+	case TagUser, TagUserObj:
+		return "user"
+	case TagGroup, TagGroupObj:
+		return "group"
+	case TagOther:
+		return "other"
+	case TagMask:
+		return "mask"
+	default:
+		// TODO(synful): what to do in this case?
+		return "????" // non-standard, but not specified in POSIX.1e
+	}
+}
+
+// Entry represents an entry in an ACL.
 type Entry struct {
 	Tag Tag
 
@@ -72,60 +219,77 @@ type Entry struct {
 
 	// The Qualifier specifies what entity (user or group)
 	// this entry applies to. If the Tag is TagUser, it is
-	// a UID; if the Tag is TagGroup, it is a GID; and otherwise
+	// a UID; if the Tag is TagGroup, it is a GID; otherwise
 	// the field is ignored.
 	Qualifier string
-	Perms     os.FileMode
+
+	// ACL permissions are taken from a traditional rwx
+	// (read/write/execute) permissions vector. The Perms
+	// field stores these as the lowest three bits -
+	// the bits in any higher positions are ignored.
+	Perms os.FileMode
 }
 
-// Used for pretty-printing
-type aclPerm os.FileMode
+// Use e.perms() to make sure that only
+// the lowest three bits are set - some
+// algorithms may inadvertently break
+// otherwise (including libacl itself).
+func (e Entry) perms() os.FileMode { return 7 & e.Perms }
 
-func (a aclPerm) String() string {
-	var buf [3]byte
-	const rwx = "rwx"
-	for i := 2; i >= 0; i-- {
-		if a&1 != 0 {
-			buf[i] = rwx[i]
-		} else {
-			buf[i] = '-'
-		}
-		a >>= 1
-	}
-	return string(buf[:])
+var permStrings = []string{
+	0: "---",
+	1: "--x",
+	2: "-w-",
+	3: "-wx",
+	4: "r--",
+	5: "r-x",
+	6: "rw-",
+	7: "rwx",
 }
 
+// assumes perm has only lowest three bits set
+func permString(perm os.FileMode) string {
+	return permStrings[int(perm)]
+}
+
+// String implements the POSIX.1e short text form.
 func (e Entry) String() string {
-	var toPrint interface{}
+	middle := "::"
 	if e.Tag == TagUser || e.Tag == TagGroup {
-		type entry struct {
-			Tag       Tag
-			Qualifier string
-			Perm      aclPerm
-		}
-		toPrint = entry{e.Tag, e.Qualifier, aclPerm(e.Perms)}
-	} else {
-		type entry struct {
-			Tag  Tag
-			Perm aclPerm
-		}
-		toPrint = entry{e.Tag, aclPerm(e.Perms)}
+		middle = ":" + e.Qualifier + ":"
 	}
-	return fmt.Sprint(toPrint)
+	return fmt.Sprintf("%s%s%s", e.Tag, middle, permString(e.perms()))
 }
 
+// StringLong implements the POSIX.1e long text form.
+func (e Entry) StringLong() string {
+	middle := "::"
+	if e.Tag == TagUser || e.Tag == TagGroup {
+		middle = ":" + e.Qualifier + ":"
+	}
+	return fmt.Sprintf("%s%s%s", e.Tag.StringLong(), middle, permString(e.perms()))
+}
+
+// Get retrieves the access ACL associated with path,
+// returning any error encountered.
 func Get(path string) (ACL, error) {
 	return get(path)
 }
 
+// GetDefault retrieves the default ACL associated with path,
+// returning any error encountered.
 func GetDefault(path string) (ACL, error) {
 	return getDefault(path)
 }
 
+// Set sets the access ACL on path,
+// returning any error encountered.
 func Set(path string, acl ACL) error {
 	return set(path, acl)
 }
 
+// SetDefault sets the default ACL on path,
+// returning any error encountered.
 func SetDefault(path string, acl ACL) error {
 	return setDefault(path, acl)
 }
--- acl_test.go
+++ acl_test.go
@@ -1,6 +1,7 @@
 package acl
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"reflect"
@@ -41,6 +42,119 @@ func TestSet(t *testing.T) {
 	acl2, err := Get(f)
 	mustNotError(t, err)
 	if !reflect.DeepEqual(acl, acl2) {
-		t.Errorf("unexpected acl: want %v; got %v", acl, acl2)
+		t.Errorf("unexpected ACL: want %v; got %v", acl, acl2)
 	}
+}
+
+var (
+	userObj  = Entry{Tag: TagUserObj}
+	groupObj = Entry{Tag: TagGroupObj}
+	other    = Entry{Tag: TagOther}
+	user     = Entry{Tag: TagUser}
+	group    = Entry{Tag: TagGroup}
+	mask     = Entry{Tag: TagMask}
+)
+
+var validACLs = []ACL{
+	{userObj, groupObj, other},
+	{userObj, groupObj, other, user, mask},
+	{userObj, groupObj, other, group, mask},
+	{userObj, groupObj, other, user, group, mask},
+	{userObj, groupObj, other, user, {Tag: TagUser, Qualifier: " "}, group, {Tag: TagGroup, Qualifier: " "}, mask},
+}
+
+var invalidACLs = []ACL{
+	{},                  // No user, group, or other entries
+	{userObj},           // No group or other entries
+	{groupObj},          // No user or other entries
+	{other},             // No user or group entries
+	{userObj, groupObj}, // No other entry
+	{userObj, other},    // No group entry
+	{groupObj, other},   // No user entry
+
+	{userObj, groupObj, other, {}},                 // Invalid tag type
+	{userObj, groupObj, other, user},               // No mask
+	{userObj, groupObj, other, group},              // No mask
+	{userObj, groupObj, other, user, user, mask},   // Duplicate user qualifiers
+	{userObj, groupObj, other, group, group, mask}, // Duplicate group qualifiers
+	{userObj, groupObj, other, user, mask, mask},   // Duplicate mask entries
+}
+
+func TestIsValid(t *testing.T) {
+	f := mustMakeTempFile(t)
+	defer os.Remove(f)
+	acl, err := Get(f)
+	mustNotError(t, err)
+	if !acl.IsValid() {
+		t.Errorf("ACL reported invalid: %v", acl)
+	}
+	for _, v := range validACLs {
+		if !v.IsValid() {
+			t.Errorf("ACL reported invalid: %v", v)
+		}
+	}
+	for _, i := range invalidACLs {
+		if i.IsValid() {
+			t.Errorf("ACL reported valid: %v", i)
+		}
+	}
+}
+
+func TestUnix(t *testing.T) {
+	aclFromUnix := func(user, group, other os.FileMode) ACL {
+		return ACL{
+			{Tag: TagUserObj, Perms: user},
+			{Tag: TagGroupObj, Perms: group},
+			{Tag: TagOther, Perms: other},
+		}
+	}
+	// Run through every possible unix permissions bitmask
+	// and make sure it is generated with ToUnix and its
+	// accompanying ACL is generated with FromUnix.
+	for user := os.FileMode(0); user < 8; user++ {
+		for group := os.FileMode(0); group < 8; group++ {
+			for other := os.FileMode(0); other < 8; other++ {
+				perms := (user << 6) | (group << 3) | other
+				acl := aclFromUnix(user, group, other)
+				permstmp := ToUnix(acl)
+				// Add in extra high bits to make sure
+				// we're only considering permissions bits
+				max := ^os.FileMode(0)
+				acltmp := FromUnix(perms | max<<9)
+				if permstmp != perms {
+					t.Errorf("unexpected perms: want %v; got %v; acl: %v", perms, permstmp, acl)
+				}
+				if !reflect.DeepEqual(acltmp, acl) {
+					t.Errorf("unexpected acl: want %v; got %v; perms: %v", acl, acltmp, perms)
+				}
+			}
+		}
+	}
+}
+
+func ExamplePrint() {
+	acl := ACL{
+		{Tag: TagUserObj, Perms: 7},
+		{Tag: TagGroupObj, Perms: 6},
+		{Tag: TagOther, Perms: 5},
+		{Tag: TagUser, Qualifier: "1", Perms: 4},
+		{Tag: TagUser, Qualifier: "2", Perms: 3},
+		{Tag: TagUser, Qualifier: "3", Perms: 2},
+		{Tag: TagGroup, Qualifier: "4", Perms: 1},
+		{Tag: TagGroup, Qualifier: "5", Perms: 0},
+		{Tag: TagMask, Perms: 5},
+	}
+	fmt.Println(acl)
+	fmt.Println(acl.StringLong())
+
+	// Output: u::rwx,g::rw-,o::r-x,u:1:r--,u:2:-wx,u:3:-w-,g:4:--x,g:5:---,m::r-x
+	// user::rwx
+	// group::rw-          #effective:r--
+	// other::r-x
+	// user:1:r--
+	// user:2:-wx          #effective:--x
+	// user:3:-w-          #effective:---
+	// group:4:--x
+	// group:5:---
+	// mask::r-x
 }
